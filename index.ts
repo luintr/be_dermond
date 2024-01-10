84be50ab6e7de24f4000f8d518b5a73131c83488
luintr <hainam.huflit@gmail.com>
2024-01-10 23:16:37 +0700

    test

--- index.ts
+++ index.ts
@@ -1,37 +1,35 @@
-import path from 'path'
-import bodyParser from 'body-parser'
-import express from 'express'
-import cookieParser from 'cookie-parser'
+import path from 'path';
+import bodyParser from 'body-parser';
+import express from 'express';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import connectDB from './src/config/db';
-import productRoute from './src/routes/productRoute'
-import userRoute from './src/routes/userRoute'
-import orderRoute from './src/routes/orderRoute'
+import productRoute from './src/routes/productRoute';
+import userRoute from './src/routes/userRoute';
+import orderRoute from './src/routes/orderRoute';
 import { errorHandler, notFound } from './src/middleware/errorHandler';
 import http from 'http';
 
-require('dotenv').config()
-const app = express()
+require('dotenv').config();
+const app = express();
 
-connectDB()
+connectDB();
 
 app.use(cors());
-// app.set("port", process.env.PORT);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API Running");
+app.get('/', (req, res) => {
+  res.send('API Running');
 });
 
-app.use('/api/products', productRoute)
-app.use('/api/users', userRoute)
-app.use('/api/orders', orderRoute)
+app.use('/api/products', productRoute);
+app.use('/api/users', userRoute);
+app.use('/api/orders', orderRoute);
 
-
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 const httpApp = http.createServer(app);
 
