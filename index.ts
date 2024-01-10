7fa0b2c106021dd8ed3ab41c1b1a51981661f5d6
luintr <hainam.huflit@gmail.com>
2024-01-10 23:07:56 +0700

    test

--- index.ts
+++ index.ts
@@ -29,7 +29,6 @@ app.use('/api/products', productRoute)
 app.use('/api/users', userRoute)
 app.use('/api/orders', orderRoute)
 
-app.get('api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }))
 
 app.use(notFound)
 app.use(errorHandler)
